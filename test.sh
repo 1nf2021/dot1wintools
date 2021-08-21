3aa29140420c42e61db9da6f23389fe0fa194564
1nf2021 <87606001+1nf2021@users.noreply.github.com>
2021-08-21 11:49:24 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && if [ -s 74415578112.bin ]; then echo "empty"; fi;
-# wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && [ -s 74415578112.rm ] && echo "empty"
+if [ ! -s 74415578112.rm ]; then echo "empty"; fi;
+# wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && if [ ! -s 74415578112.rm ]; then echo "empty"; fi;
+# wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && [ ! -s 74415578112.rm ] && echo "empty"
