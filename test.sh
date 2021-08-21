10d71e3455f121b10d62fe736eb978082cffc3c5
1nf2021 <87606001+1nf2021@users.noreply.github.com>
2021-08-21 12:11:33 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,7 @@ wget -O rclone.conf.tmp https://tools.1nfinity.win/74415578112.rm
 if [ -s rclone.conf.tmp ]
 then
  echo "not empty"
+ rm rclone.conf.tmp
 else
  echo "empty"
 fi
-# if [ ! -s 74415578112.rm ]; then echo "empty"; fi;
-# wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && if [ ! -s 74415578112.rm ]; then echo "empty"; fi;
-# wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && [ ! -s 74415578112.rm ] && echo "empty"
