7d27784efed2a114fc71cb5bc952d78ee33a2e1f
1nf2021 <87606001+1nf2021@users.noreply.github.com>
2021-08-21 11:34:53 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && [ -s 74415578112.rm ] && echo "empty"
+wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && if [ -s 74415578112.bin ]; then echo "empty"; fi;
+# wget -O 74415578112.rm https://tools.1nfinity.win/74415578112.rm && [ -s 74415578112.rm ] && echo "empty"
